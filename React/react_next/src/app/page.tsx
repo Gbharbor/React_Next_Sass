2064ba0ac1e8e3ca28281a20eb72422270cb2d2a
Drportox <guiportopt@outlook.com>
2026-02-27 17:40:38 +0000

    chore:reorganize project & separate Next and Vite apps.

--- React/react_next/src/app/page.tsx
+++ React/react_next/src/app/page.tsx
@@ -20,6 +20,12 @@ const Page = () => {
       });
       setAddField('');
    }
+   const handleDoneCheckbox =(id: number) => {
+      dispatch({
+         type: 'toggleDone',
+         payload: {id}
+      });
+   }
    return (
       <div className="container mx-auto">
          <h1 className="text-center text-4xl my-4">Lista de Tarefas</h1>
@@ -39,10 +45,21 @@ const Page = () => {
             </button>
             
          </div>
-         <ul>
+         <ul className="max-w-2xl mx-auto">
             {list.map(item =>(
-               <li key={item.id}>{item.text}</li>
-
+               <li 
+               key={item.id}
+               className="flex items-center p-3 my-3 border-b border-gray-700"
+               >
+                  <input type="checkbox"
+                  className="w-6 h-6 mr-4"
+                  checked={item.done}
+                  onClick={()=>handleDoneCheckbox(item.id)}
+                  />
+                  <p className="flex-1 text-lg">{item.text}</p>
+                  <button className="mx-4 text-white hover:text-gray-500" >Editar</button>
+                  <button className="mx-4 text-white hover:text-gray-500">Excluir</button>
+               </li>
             ))}
          </ul>
       </div>
